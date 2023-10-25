57ec9c75d22bc007e4f3eb87255ccbb261db4853
alexander.alvarez <alexander.alvarez@sqasa.co>
2023-10-25 14:21:58 -0500

    commit 2 para el reto

---
+++ src/main/java/com/amazon/tasks/AmazonArticuloTask2.java
@@ -0,0 +1,11 @@
+package com.amazon.tasks;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+
+public class AmazonArticuloTask2 implements Task {
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+
+    }
+}
