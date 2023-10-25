f0daf211878c9d87dfa739e4582a5db14b9cf696
alexander.alvarez <alexander.alvarez@sqasa.co>
2023-10-25 14:43:55 -0500

    commit 3 para el reto

---
+++ src/main/java/com/amazon/utils/BD.java
@@ -0,0 +1,17 @@
+package com.amazon.utils;
+
+public class BD {
+
+    public static void nameMethod(){
+
+    }
+
+    public void getInfo(){
+
+    }
+
+    public void setInfo(){
+        
+    }
+
+}
